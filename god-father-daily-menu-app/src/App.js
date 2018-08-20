9333f289ad3d0d32d08763e7ef1a4ecee076ac07
michael.lau <michael.lau@sinodynamic.com>
2018-08-20 16:26:56 +0800

    update
    update

--- god-father-daily-menu-app/src/App.js
+++ god-father-daily-menu-app/src/App.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Parser from 'rss-parser';
+
 
 class App extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+
+
+
+    this.state = {
+      mainContent: [],
+      title:'A'
+    };
+
+    this.getFacebookContent = this.getFacebookContent.bind(this);
+
+  }
+  componentDidMount() {
+    this.getFacebookContent();
+  }
+
+  getFacebookContent() {
+    let self = this;
+    let parser = new Parser();
+    this.state.title ="XXXXX";
+    parser.parseURL('http://nullmicgo.com/lunchtime/server.php')
+    .then(feed => {
+        feed.items.forEach(function(entry) {
+          console.log(entry.title);
+
+          this.setState({ title: entry.title });
+
+        });
+    });
+
+
+  }
+
   render() {
+    //  let containerContent = this.state.mainContent.map((form,i) => {
+    //    console.log('test >'+form);
+    //    return 'asdasdasd';
+    // }); 
+  
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +55,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           { this.state.title }
         </p>
       </div>
     );
